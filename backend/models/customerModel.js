54655f83a290fea24276249039afc0aa34c4af2b
Anderson Asprilla <anderson.asprilla@gmail.com>
2023-08-17 21:58:37 -0400

    Modeling Data

---
+++ backend/models/customerModel.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+
+const customerSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    tag: {
+      type: Number,
+      required: true,
+    },
+    ro: {
+      type: Number,
+      required: true,
+    },
+    vehicle: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: Number,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Customer = mongoose.model("Customer", customerSchema);
+
+export default Customer;
