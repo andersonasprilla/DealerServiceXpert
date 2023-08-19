e40d8db0856050fb373ec9fb6742f7846965fdb7
Anderson Asprilla <anderson.asprilla@gmail.com>
2023-08-19 19:06:33 -0400

    Redux Store and Api Slice

---
+++ frontend/src/constants.js
@@ -0,0 +1,4 @@
+export const BASE_URL =
+  process.env.NODE_ENV === "development" ? "http://localhost:5001" : "";
+export const CUSTOMERS_URL = `api/customers`;
+export const USERS_URL = `api/users`;
