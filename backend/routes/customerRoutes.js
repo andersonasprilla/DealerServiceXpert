2f12864e037ec42f5dfc5c91cac8b0ddb7347f1d
Anderson Asprilla <anderson.asprilla@gmail.com>
2023-08-18 20:58:01 -0400

    Custome Error Middleware

---
+++ backend/routes/customerRoutes.js
@@ -0,0 +1,30 @@
+import express from "express";
+const router = express.Router();
+import asyncHandler from "../middleware/asyncHandler.js";
+import Customer from "../models/customerModel.js";
+
+router.get(
+  "/",
+  asyncHandler(async (req, res) => {
+    const customers = await Customer.find({});
+    throw new Error("Some Error");
+
+    res.json(customers);
+  })
+);
+
+router.get(
+  "/:id",
+  asyncHandler(async (req, res) => {
+    const customer = await Customer.findById(req.params.id);
+
+    if (customer) {
+      return res.json(customer);
+    }
+
+    res.status(404);
+    throw new Error("Resource not found");
+  })
+);
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import connectDB from "./config/db.js";
-import customers from "./data/customers.js";
+import customerRoutes from "./routes/customerRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 const port = process.env.PORT || 5000;
 
 connectDB();
@@ -13,14 +14,11 @@ app.get("/", (req, res) => {
   res.send("Api is Running...");
 });
 
-app.get("/api/customers", (req, res) => {
-  res.json(customers);
-});
+app.use("/api/customers", customerRoutes);
 
-app.get("/api/customers/:id", (req, res) => {
-  const customer = customers.find((c) => c._id === req.params.id);
-  res.json(customer);
-});
+app.use(notFound);
+
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
